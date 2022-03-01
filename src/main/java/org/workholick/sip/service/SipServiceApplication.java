cc69a28376421544da9cd32bcf6be667ea55fb42
workholicc <workholicc@gmail.com>
2022-03-01 16:02:05 +0530

    Initial sip-service

---
+++ src/main/java/org/workholick/sip/service/SipServiceApplication.java
@@ -0,0 +1,13 @@
+package org.workholick.sip.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SipServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SipServiceApplication.class, args);
+	}
+
+}
